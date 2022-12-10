5c2a324747cafdde3ad1f4673ea7c74e986f1a60
yrolland <yann.rolland@iadvize.com>
2022-12-10 10:42:14 +0100

    promising

--- src/main/scala/Main7Part1.scala
+++ src/main/scala/Main7Part1.scala
@@ -1,5 +1,7 @@
 import Helpers._
 
+import scala.collection.mutable
+
 object Main7Part1 {
 
   sealed trait History extends Product
@@ -11,46 +13,6 @@ object Main7Part1 {
   sealed trait Output extends History
   case class FileSizeOutput(size: Long, name: String) extends Output
   case class DirOutput(name: String) extends Output
-
-  sealed trait Tree
-
-  case class File(name: String, size: Long) extends Tree {
-    override def toString: String = {
-      s"$name ($size)"
-    }
-  }
-
-  case class Directory(name: String, files: List[File], dirs: List[Directory])
-      extends Tree {
-    override def toString: String = {
-      // tree as indented string
-      val filesStr =
-        if (files.isEmpty) ""
-        else files.map(_.toString).map("  " + _).mkString("\n") + "\n"
-      val dirsStr =
-        if (dirs.isEmpty) ""
-        else
-          dirs
-            .map(_.toString.split("\n").map("  " + _).mkString("\n"))
-            .mkString("\n")
-      s"$name\n$filesStr$dirsStr"
-    }
-  }
-
-  def totalFileSize(tree: Tree): Long = {
-    tree match {
-      case File(_, size) => size
-      case Directory(_, files, dirs) =>
-        files.map(_.size).sum + dirs.map(totalFileSize).sum
-    }
-  }
-
-  def findDirectorySizes(directory: Directory): Map[String, Long] = {
-    directory.dirs
-      .map(findDirectorySizes)
-      .flatten
-      .toMap + (directory.name -> totalFileSize(directory))
-  }
 
   def main(args: Array[String]): Unit = {
     val lines = loadLines("input7.txt")
@@ -65,78 +27,40 @@ object Main7Part1 {
             other.dropWhile(_.isDigit).drop(1)
           )
       }
-      .drop(1)
     println(parsed)
+    val allSizes = findAllDirectorySizes(parsed)
+    println(allSizes)
 
-    val builtTree = parsed
-      .foldLeft(
-        (Directory("/", List.empty, List.empty), List.empty[String])
-      )((acc, next) => {
-        val stepRes = (acc, next) match {
-          case (acc, Ls) => acc
-          case ((tree, currentPath), cd: Cd) if cd.path == ".." =>
-            (tree, currentPath.dropRight(1))
-          case ((tree, currentPath), cd: Cd) => (tree, currentPath :+ cd.path)
-          case ((tree, currentPath), fileSizeOutput: FileSizeOutput) =>
-            (updated(tree, currentPath, fileSizeOutput), currentPath)
-          case ((tree, currentPath), dirOutput: DirOutput) =>
-            (updated(tree, currentPath, dirOutput), currentPath)
-        }
-//        println(
-//          s"at ${acc._2} with ${next} got path ${stepRes._2} and tree\n${stepRes._1}"
-//          s"at ${acc._2} with ${next} got path ${stepRes._2}"
-//        )
-        stepRes
-      })
+    val smallSizes = allSizes.filter(_._2 < 100000)
+    println(smallSizes)
 
-    println("result:\n" + builtTree._1)
+    val smallSizesSum = smallSizes.map(_._2).sum
+    println(smallSizesSum)
 
-    val directorySizes = findDirectorySizes(builtTree._1)
-
-    println("directory sizes:\n" + directorySizes)
-
-    val smallDirs = directorySizes.filter(_._2 <= 100000)
-
-    println("small dirs:\n" + smallDirs)
-    println(smallDirs.values.sum)
+    // not 1075948
   }
 
-  def updated(
-      directory: Directory,
-      path: List[String],
-      output: Output
-  ): Directory = {
-//    println(s"updating ${directory.name} with $output at $path")
+  /** Find all directory sizes from history, a file contributes to the size of all its parent directories. */
+  def findAllDirectorySizes(history: List[History]): Map[String, Long] = {
+    val directorySizes = mutable.Map.empty[String, Long]
+    val currentDirectory = mutable.ListBuffer.empty[String]
 
-    (path, output) match {
-      case (Nil, _) =>
-        output match {
-          case FileSizeOutput(size, name) =>
-            println(s"updating leaf with file $name")
-            directory.copy(files = directory.files :+ File(name, size))
-          case DirOutput(name) =>
-            println(s"updating leaf with dir $name")
-            directory.copy(dirs =
-              directory.dirs :+ Directory(name, List.empty, List.empty)
-            )
+    history.foreach {
+      case Cd("..") =>
+        currentDirectory.remove(currentDirectory.size - 1)
+      case Cd(path) =>
+        currentDirectory += path
+      case Ls =>
+      // do nothing
+      case FileSizeOutput(size, name) =>
+        val allParentDirectories = currentDirectory.toList
+        allParentDirectories.foreach { dir =>
+          directorySizes(dir) = directorySizes.getOrElse(dir, 0L) + size
         }
-      case (head :: tail, _) =>
-        directory.dirs.find(_.name == head) match {
-          case Some(dir) =>
-            directory.copy(
-              dirs = directory.dirs.map(d =>
-                if (d.name == head) updated(d, tail, output) else d
-              )
-            )
-          case None =>
-            directory.copy(
-              dirs = directory.dirs :+ updated(
-                Directory(head, List.empty, List.empty),
-                tail,
-                output
-              )
-            )
-        }
+      case DirOutput(name) =>
+      // do nothing
     }
+
+    directorySizes.toMap
   }
 }
