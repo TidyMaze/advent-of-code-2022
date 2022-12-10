90e5eb6f01aa18ab8e9bb8a4da95efac59cc7731
yrolland <yann.rolland@iadvize.com>
2022-12-10 11:02:50 +0100

    wip

---
+++ src/main/scala/Main7Part2.scala
@@ -0,0 +1,80 @@
+import Helpers._
+
+import scala.collection.mutable
+
+object Main7Part2 {
+
+  sealed trait History extends Product
+
+  sealed trait Command extends History
+  case class Cd(path: String) extends Command
+  case object Ls extends Command
+
+  sealed trait Output extends History
+  case class FileSizeOutput(size: Long, name: String) extends Output
+  case class DirOutput(name: String) extends Output
+
+  def main(args: Array[String]): Unit = {
+    val lines = loadLines("input7.txt")
+    val parsed = lines
+      .map {
+        case str if str.startsWith("$ cd ") => Cd(str.drop(5))
+        case str if str == "$ ls"           => Ls
+        case str if str.startsWith("dir ")  => DirOutput(str.drop(4))
+        case other =>
+          FileSizeOutput(
+            other.takeWhile(_.isDigit).toLong,
+            other.dropWhile(_.isDigit).drop(1)
+          )
+      }
+    println(parsed)
+    val allSizes = findAllDirectorySizes(parsed)
+    println(allSizes)
+
+    val TotalDiskSpace = 70000000
+    val RequiredDiskSpace = 30000000
+
+    val totalCurrentTakenSpace = allSizes("/")
+    println("totalCurrentTakenSpace: " + totalCurrentTakenSpace)
+
+    val minimalSpaceToDelete =
+      RequiredDiskSpace - (TotalDiskSpace - totalCurrentTakenSpace)
+    println("minimalSpaceToDelete: " + minimalSpaceToDelete)
+
+    val smallestDirectoryToDelete =
+      allSizes.toList.sortBy(_._2).find(_._2 >= minimalSpaceToDelete).get
+    println("smallestDirectoryToDelete: " + smallestDirectoryToDelete)
+  }
+
+  /** Find all directory sizes from history, a file contributes to the size of all its parent directories. */
+  def findAllDirectorySizes(history: List[History]): Map[String, Long] = {
+    val directorySizes = mutable.Map.empty[String, Long]
+    val currentDirectory = mutable.ListBuffer.empty[String]
+
+    history.foreach {
+      case Cd("..") =>
+        currentDirectory.remove(currentDirectory.size - 1)
+      case Cd(path) =>
+        currentDirectory += path
+      case Ls =>
+      // do nothing
+      case FileSizeOutput(size, name) =>
+        // list of all parent directory paths (ex: List("a", "b", "c") -> List("a", "a/b", "a/b/c"))
+        val allParentDirectoriesPaths = currentDirectory
+          .scanLeft("")((acc, next) =>
+            acc match {
+              case ""  => next
+              case "/" => s"/$next"
+              case _   => acc + "/" + next
+            }
+          )
+        allParentDirectoriesPaths.foreach { dir =>
+          directorySizes(dir) = directorySizes.getOrElse(dir, 0L) + size
+        }
+      case DirOutput(name) =>
+      // do nothing
+    }
+
+    directorySizes.toMap
+  }
+}
